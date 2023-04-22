a4d3b08a603c3e501d1151e08c9f4348a01f6a61
Baekshook <ecgo0106@naver.com>
2023-04-22 17:53:56 +0900

    enter키 누르면 추가되는 기능 구현_2

--- src/components/molcules/AddNames.jsx
+++ src/components/molcules/AddNames.jsx
@@ -16,27 +16,22 @@ export default function AddNames({myName, setMyName}) {
 
   return (
     <>
-      <Input
-        mt={"20px"}
-        ml={"20px"}
-        htmlSize={20}
-        width="auto"
-        placeholder="이름을 입력하세요."
-        isInvalid
-        errorBorderColor="black"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      ></Input>
-      <Button
-        ml={"20px"}
-        onClick={() => {
-          onAddNames(name);
-          //이름은 다시 ''초기화
-          setName("");
-        }}
-      >
-        추가
-      </Button>
+      <form onSubmit={onAddNames}> 
+        <Input
+          mt={"20px"}
+          ml={"20px"}
+          htmlSize={20}
+          width="auto"
+          placeholder="이름을 입력하세요."
+          isInvalid
+          errorBorderColor="black"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        ></Input>
+        <Button ml={"20px"} type="submit">
+          추가
+        </Button>
+      </form>
     </>
   );
 }
